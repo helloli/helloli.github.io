10726422969d1a08203ff2d678e8d0a4672cfc2b
lizhenghua <lizhenghua@360.cn>
2016-11-30 15:15:54 +0800

    update

---
+++ meiyaNode/dao/picSqlMapping.js
@@ -0,0 +1,15 @@
+// dao/picSqlMapping.js
+// CRUD SQL语句
+// 九图的每次获取十条数据，需要传最后一条数据的id
+// 单图的每次获取十天的数据
+var pic = {
+    // getNinePic: 'select * from ninepic where ',
+    insert: 'INSERT INTO user(username, password) VALUES(?, ?)',
+    update: 'update user set username=?, password=? where id=?',
+    delete: 'delete from user where username=?',
+    queryByUsername: 'select * from user where username=?',
+    queryAll: 'select * from user',
+    checkPassword: 'select * from user where username=? and password=?'
+};
+
+module.exports = pic;
