4e8c1c3d130b0ca29054c732c24c912ce9439422
lizhenghua <lizhenghua@360.cn>
2016-12-06 20:07:13 +0800

    update

--- meiyaNode/dao/picSqlMapping.js
+++ meiyaNode/dao/picSqlMapping.js
@@ -3,8 +3,13 @@
 // 九图的每次获取十条数据，需要传最后一条数据的id
 // 单图的每次获取十天的数据
 var pic = {
+    // 获取九图
     getNinePic: 'select * from ninepic where unix_timestamp(addTime)*1000 < ? order by addTime desc limit ?,?',
+    // 获取广告地址
     getAdPic: 'select * from ads order by addTime desc limit 1',
+    // 获取野表情
+    // getWildPic: 'select * from wildpic '
+
     insert: 'INSERT INTO user(username, password) VALUES(?, ?)',
     update: 'update user set username=?, password=? where id=?',
     delete: 'delete from user where username=?',
