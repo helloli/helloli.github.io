01181e4ac66458ec03c591636fdb5010cf7e9d73
lizhenghua <lizhenghua@360.cn>
2016-12-19 09:54:21 +0800

    update

---
+++ node/index.js
@@ -0,0 +1,18 @@
+// var isTrue = function(value, callback) {
+//   if (value === true) {
+//     callback(null, "Value was true.");
+//   }
+//   else {
+//     callback(new Error("Value is not true!"));
+//   }
+// }
+// var callback = function (error, value) {
+//   if (error) {
+//     return console.log(error);  
+//   }
+//   console.log(value);
+// }
+
+// isTrue(true, callback);
+
+var x = 1;
