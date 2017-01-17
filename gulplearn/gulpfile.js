e23cd512680a2f383dc713271a41636c82492180
lizhenghua <lizhenghua@360.cn>
2017-01-17 16:52:52 +0800

    update

--- gulplearn/gulpfile.js
+++ gulplearn/gulpfile.js
@@ -2,16 +2,23 @@
 var gulp = require('gulp'),
     yaml = require('js-yaml'),
     fs = require('fs'),
-    path = require('path')
+    path = require('path'),
+    del = require('del')
 
 // 测试开始
-try {
-    var doc = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '/_config.yml'), 'utf8'));
-    console.log(doc);
-} catch (e) {
-    console.log(e);
-}
+// try {
+//     var doc = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '/_config.yml'), 'utf8'));
+//     console.log(doc);
+// } catch (e) {
+//     console.log(e);
+// }
 
-gulp.task('default', function() {
-  // 做一些事
-});
+gulp.task('cleanRoot', function (done) {
+    return del('./amd/*', {
+        force: true
+    });
+})
+
+// gulp.task('')
+
+gulp.task('default', ['cleanRoot', '']);
